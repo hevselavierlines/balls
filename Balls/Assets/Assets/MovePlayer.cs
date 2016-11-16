73d5662bc7338d52fc2d660fcc9574efc90bf4ab
Afaci <baumi494@gmail.com>
2016-11-16 16:42:09 +0100

    added arrow

--- Balls/Assets/Assets/MovePlayer.cs
+++ Balls/Assets/Assets/MovePlayer.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MovePlayer : MonoBehaviour {
 	private Rigidbody rb;
 	public float speed;
+	public Image directionArrow;
 	private long time;
 	private Vector3 startPos;
 	private int notMoving = -1;
+	private float angle = 0.0f;
 
 	void Start() {
 		rb = GetComponent<Rigidbody> ();
 		time = 0;
 		startPos = transform.position;
+		directionArrow.canvasRenderer.SetAlpha (1.0f);
 	}
 
 	void FixedUpdate() {
@@ -24,6 +28,7 @@ public class MovePlayer : MonoBehaviour {
 			print ("Not moving " + notMoving);
 			if (notMoving > 60) {
 				notMoving = -1;
+				directionArrow.canvasRenderer.SetAlpha (1.0f);
 			}
 		}
 		if (notMoving == -1) {
@@ -31,12 +36,20 @@ public class MovePlayer : MonoBehaviour {
 			rb.angularVelocity = Vector3.zero;
 			if (Input.GetKey ("up")) {
 				time++;
+			} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+				angle += 0.1f;
+				directionArrow.transform.Rotate(new Vector3(0.0f,0.0f,angle));
+			} else if(Input.GetKeyDown(KeyCode.RightArrow)) {
+				angle -= 0.1f;
+				directionArrow.transform.Rotate(new Vector3(0.0f,0.0f,angle));
 			} else if (Input.GetKeyUp (KeyCode.UpArrow)) {
-				Vector3 movement = new Vector3 (0.0f, 0.0f, time * speed);
+				Vector3 movement = new Vector3 (Mathf.Sin(angle) * time * speed, 0.0f, Mathf.Cos(angle) * time * speed);
 				rb.AddForce (movement);
 				time = 0;
 				notMoving = 0;
+				directionArrow.canvasRenderer.SetAlpha (0.0f);
 			}
+
 		}
 
 		if (transform.position.y < 0.0) {
@@ -55,5 +68,6 @@ public class MovePlayer : MonoBehaviour {
 		rb.angularVelocity = Vector3.zero; 
 		time = 0;
 		notMoving = -1;
+		directionArrow.canvasRenderer.SetAlpha (1.0f);
 	}
 }
